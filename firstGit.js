1c9cbe0c2019656c005b1b4704bd4e572feefb02
Lqy <trinyoung@163.com>
2017-09-05 14:21:43 +0800

    append GPL

--- firstGit.js
+++ firstGit.js
@@ -3,4 +3,4 @@
  */
 var first="this a new file for git";
 var second="Git is a distributed version control system";
-var third=" Git is free software";
+var third=" Git is free software under the GPL";
