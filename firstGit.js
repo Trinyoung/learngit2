2de3a09be2161764ec8de9c5b57d218c7bfb701f
Lqy <trinyoung@163.com>
2017-09-05 14:19:49 +0800

    add distributed

--- firstGit.js
+++ firstGit.js
@@ -2,5 +2,5 @@
  * Created by 12098 on 2017/9/5.
  */
 var first="this a new file for git";
-var second="Git is a version control system";
+var second="Git is a distributed version control system";
 var third=" Git is free software";
