323a1967ad0f3e71dd0e3c500df9ee74b7990c1d
trinyoung <trinyoung@163.com>
2020-07-31 09:50:57 +0800

    update .gitignore

--- firstGit.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by 12098 on 2017/9/5.
- */
-var first="this a new file for git";
-var second="Git is a distributed version control system";
-var third=" Git is free software under the GPL";
-var fifth="Git new banch";
-var sixth="Creating a new branch is quick & simple."
-var seventh= "test gitignore"
